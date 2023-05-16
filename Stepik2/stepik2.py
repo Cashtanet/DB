8cb7bc35d0e323360f62fc5dc1583b6f5aab9b4b
Aleksandr Tyrchenkov <cashtanet@mail.ru>
2023-05-16 15:00:04 +0300

    sets

--- Stepik2/stepik2.py
+++ Stepik2/stepik2.py
@@ -734,39 +734,586 @@
 
 
 ## 4.5.4
+# n = int(input())
+# matrix = [input().split() for _ in range(n)]
+#
+# for i in range(n):
+#     # for k in range(n):
+#     matrix[i][i], matrix[n - i - 1][i] = matrix[n - i - 1][i], matrix[i][i]
+#
+# for r in range(n):
+#     print(*matrix[r])  # !!!!!запомни уже
+
+
+## 4.5.5
+# n = int(input())
+# matrix = [input().split() for _ in range(n)]
+# for i in range(n - 1, -1, -1):
+#     print(*matrix[i])
+
+
+## 4.5.6
+# n = int(input())
+# matrix = [input().split() for _ in range(n)]
+#
+# for i in range(n):
+#     for j in range(n):
+#         print((matrix[n - j - 1][i]), end=" ")
+#     print()
+
+
+##4.5.7 #ход коня на поле после ввода клетки типа f5
+# matrix = [['.' for _ in range(8)] for _ in range(8)]
+# char = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+# num = {'1': 7, '2': 6, '3': 5, '4': 4, '5': 3, '6': 2, '7': 1, '8': 0}
+#
+# n = [_ for _ in input()]
+# matrix[num[n[1]]][char[n[0]]] = "N"
+#
+# for i in char:
+#     for k in num:
+#         if (char[i] - char[n[0]])**2 + (num[k]- num[n[1]])**2 == 5:
+#             matrix[num[k]][char[i]] = "*"
+#
+# for k in range(8):
+#     print(*matrix[k])
+
+
+##4.5.8
+# n = int(input())
+# matrix = [input().split() for _ in range(n)]
+# flag = "YES"
+# k = 0
+# for _ in range(n):
+#     k += int(matrix[0][int(_)])
+#
+# for i in range(n):
+#     for j in range(n):
+
+
+##4.6.1
+# s = input()
+# n, m = int(s.split()[0]), int(s.split()[1])
+# matrix = []
+# for i in range(n):
+#     matrix.append([])
+#     for j in range(m):
+#         if (i + j)%2 == 0:
+#             matrix[i].append(".")
+#         else:
+#             matrix[i].append("*")
+#
+# for r in range(n):
+#     print(*matrix[r])
+
+
+##4.6.2
+# n = int(input())
+# matrix = []
+# for i in range(n):
+#     matrix.append([])
+#     for j in range(n):
+#         if i == n - j - 1:
+#             matrix[i].append("1")
+#         elif i < n - j - 1:
+#             matrix[i].append("0")
+#         else:
+#             matrix[i].append("2")
+#
+# for r in range(n):
+#     print(*matrix[r])
+
+
+##4.6.3
+# n, m = [int(i) for i in input().split()]
+# matrix = []
+# for i in range(n):
+#     matrix.append([])
+#     for j in range(1, m + 1):
+#         matrix[i].append(i * m + j)
+#
+# for r in range(n):
+#     for p in range(m):
+#         print(str(matrix[r][p]).ljust(3), end=" ")
+#     print()
+
+
+##4.6.4
+# n, m = [int(i) for i in input().split()]
+# matrix = []
+# for i in range(n):
+#     matrix.append([])
+#     for j in range(m):
+#         matrix[i].append(i + j * n + 1)
+#
+# for r in range(n):
+#     for p in range(m):
+#         print((str(matrix[r][p])).ljust(3), end=" ")
+#     print()
+
+
+##4.6.5
+# n = int(input())
+# matrix = [[0]*n for _ in range(n)]
+#
+# for i in range(n):
+#     for j in range(n):
+#         if i == j or i == (n - j - 1):
+#             matrix[i][j] = "1"
+#
+# for r in range(n):
+#     print(*matrix[r])
+
+
+##4.6.6
+# n = int(input())
+# matrix = [[1]*n for _ in range(n)]
+#
+# for i in range(n):
+#     for j in range(n):
+#         if i < j and i > n - j - 1 or i > j and i < n - j - 1:
+#             matrix[i][j] = "0"
+#
+# for r in range(n):
+#     print(*matrix[r])
+
+
+##4.6.7 # заполнение со смещением
+# n, m = [int(i) for i in input().split()]
+# matrix = []
+# for i in range(n):
+#     matrix.append([])
+#     for j in range(m):
+#         if j + i <= m - 1:
+#             matrix[i].append(j + i + 1)
+#         else:
+#             matrix[i].append((i + j) % m + 1)
+#
+# for r in range(n):
+#     print(*matrix[r])
+
+
+##4.6.8
+# n, m = [int(i) for i in input().split()]
+# matrix = []
+# for i in range(n):
+#     matrix.append([])
+#     for j in range(1, m + 1):
+#         if i%2 == 0:
+#             matrix[i].append(i * m + j)
+#         else:
+#             matrix[i].append(i * m + m - j + 1)
+#
+# for r in range(n):
+#     for p in range(m):
+#         print(str(matrix[r][p]).ljust(3), end=" ")
+#     print()
+
+
+##4.6.9  # заполнение диагоналями
+# n, m = [int(i) for i in input().split()]
+# matrix = [["."]*m for _ in range(n)]
+#
+# num = 0
+# for q in range(n * m):
+#     for i in range(n):
+#         for j in range(m):
+#             if i + j == q:
+#                 num += 1
+#                 matrix[i][j] = num
+#
+# for r in range(n):
+#     for p in range(m):
+#         print(str(matrix[r][p]).ljust(3), end=" ")
+#     print()
+
+
+## 4.6.10!!!!!!!!!!!! #Заполнение по спирали
+# n, m = map(int, input().split())
+# a = [[0] * m for _ in range(n)]
+# dr, dc, r, c = 0, 1, 0, 0
+#
+# for cnt in range(1, n * m + 1):
+#     a[r][c] = cnt
+#
+#     if a[(r + dr) % n][(c + dc) % m]:
+#         dr, dc = dc, -dr
+#
+#     r += dr
+#     c += dc
+#
+# for row in a:
+#     print(*(f'{e:<3}' for e in row), sep='')
 
 
 
+## разобраться!"!!!
+# [n, m] = [int(num) for num in input().split()]
+# matr = [[0] * m for _ in range(n)]
+# w = 1
+# for k in range(min(n // 2 + 1, m //2 + 1)):  # количество оборотов
+#     for j in range(k, m - k):  # заполнение вправо
+#         if matr[k][j] == 0:  # условие избавляет от перезаписывания значений
+#             matr[k][j] = w
+#             w += 1
+#     for i in range(1 + k, n - k):  # заполнение вниз
+#         if matr[i][m - k - 1] == 0:
+#             matr[i][m - k - 1] = w
+#             w += 1
+#     for j in range(m - k - 2, k - 1, -1):  # заполнение влево
+#         if matr[n - k - 1][j] == 0:
+#             matr[n - k - 1][j] = w
+#             w += 1
+#     for i in range(n - k - 2, k, -1):  # заполнение вверх
+#         if matr[i][k] == 0:
+#             matr[i][k] = w
+#             w += 1
+# for i in range(n):  # вывод матрицы
+#     for j in range(m):
+#         print(str(matr[i][j]).ljust(3), end=' ')
+#     print()
+
+
+## 4.7.1 # сложение матриц
+# [n, m] = [int(num) for num in input().split()]
+# matrix1 = [input().split() for _ in range(n)]
+# input()
+# matrix2 = [input().split() for _ in range(n)]
+# matrix_res = []
+# for i in range(n):
+#     matrix_res.append([])
+#     for j in range(m):
+#         matrix_res[i].append(int(matrix1[i][j]) + int(matrix2[i][j]))
+#
+# for i in range(n):  # вывод матрицы
+#     for j in range(m):
+#         print(str(matrix_res[i][j]), end=' ')
+#     print()
+
+
+## 4.7.2 #перемножение матриц
+# [n, k1] = [int(num1) for num1 in input().split()]
+# matrix1 = [input().split() for _ in range(n)]
+# input()
+# [k2, m] = [int(num2) for num2 in input().split()]
+# matrix2 = [input().split() for _ in range(k2)]
+#
+# k = k1
+#
+# matrix_res = [[int("0")]*m for _ in range(n)]
+#
+# for i in range(n):
+#     for j in range(m):
+#         for r in range(k):
+#             matrix_res[i][j] += int(matrix1[i][r]) * int(matrix2[r][j])
+#
+# for _ in range(n):
+#     print(*matrix_res[_])
 
 
 
+## 4.7.3 # возведение квадратной матрицы в степень m
+#
+# n = int(input())
+# matrix1 = [input().split() for _ in range(n)]
+# m = int(input())
+#
+# matrix_res = [[int("0")]*n for _ in range(n)]
+#
+# for i in range(n):
+#     for j in range(n):
+#         for k in range(m):
+#             matrix_res[i][j] += int(matrix1[i][k]) * int(matrix2[k][j])
+#         # matrix[i][j] +=
+# for _ in range(n):
+#     print(*matrix_res[_])
+
+
+## 6.2
+
+# a = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+# b = 0
+# for i in range(len(a)):
+#     b += a[i]
+# print(b)
+# print(sum(a))
+
+## 6.2.11
+# city_name = input()
+# city_year = int(input())
+# city = (city_name, city_year)
+# print(city)
+
+
+## 6.2.12
+# tuples = [(), (), ('',), ('a', 'b'), (), ('a', 'b', 'c'), (1,), (), (), ('d',), ('', ''), ()]
+# non_empty_tuples = [tuples[i] for i in range(len(tuples)) if len(tuples[i]) > 0]
+#
+# print(non_empty_tuples)
+
+
+## 6.2.13
+# tuples = [(10, 20, 40), (40, 50, 60), (70, 80, 90), (10, 90), (1, 2, 3, 4), (5, 6, 10, 2, 1, 77)]
+# for i in range(len(tuples)):
+#     tuples[i] = list(tuples[i])
+#     tuples[i][-1] = 100
+#     tuples[i] = tuple(tuples[i])
+# new_tuples = tuples
+# print(new_tuples)
+
+##6.3
+
+# a = 'asdfasdfszdgasd'
+# b = list(a)
+# c = tuple(a)
+# b.sort()
+# print(b)
+# print(tuple(sorted(c)))
+
+
+## 6.3.4
+# numbers = ((10, 10, 10, 12), (30, 45, 56, 45), (81, 80, 39, 32), (1, 2, 3, 4), (90, 10))
+# a = [sum(i) / len(i) for i in numbers]
+# print(a)
+
+
+## 6.3.5
+# a, b, c = int(input()), int(input()), int(input())
+# x = (- b) / (2 * a)
+# y = (4 * a * c - b ** 2) / (4 * a)
+# coordinates = (x, y)
+# print(coordinates)
+
+
+## 6.3.6
+# n = int(input())
+# s = []
+# for _ in range(n):
+#     s.append(input().split())
+#
+# for _ in range(n):
+#     print(*s[_])
+# print()
+#
+# s1 = []
+#
+# for _ in range(n):
+#     if int(s[_][1]) > 3:
+#         s1.append(s[_])
+#
+# for _ in range(len(s1)):
+#     print(*s1[_])
+
+
+##6.3.7 упаковка и распаковка кортежей
+# a = 1, 2, 3
+# b = 5,
+# print(a)
+# print(b)
+#
+# c, d, e = a
+# print(c)
+# print(d)
+# print(e)
+
+
+##6.3.last.  Трибоначчи
+# n = int(input())
+# a = 1
+# b = 1
+# c = 1
+#
+# for i in range(n):
+#     print(a, end=' ')
+#     a, b, c = b, c, a + b + c
 
 
 
+## 8.2.1
+# m, n, k, x, y, z = [int(input()) for i in range(6)]
+# s = z + m + n + k - x - y
+# print(s)
+
+## 8.2.2
+# n, m, k, x, y, z, t, a = [int(input()) for i in range(8)]
+# s1 = (k - n - m + t) + (n - k - m + t) + (m - k - n + t)
+# s2 = x - (n - k - m + t) - (m - k - n + t) - t
+# s0 = a - x - (k - n - m + t)
+# print(s1)
+# print(s2)
+# print(s0)
+
+
+# a = 'asbcdef'
+# b = [1, 2, 3]
+# c = (1, 2, 3)
+# d = {1, 2, 3}
+# e = {1: 2, 2: 3}
+# f = 1, 23, 2
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+
+##8.4.3
+# numbers = {9089, -67, -32, 1, 78, 23, -65, 99, 9089, 34, -32, 0, -67, 1, 11, 111, 111, 1, 23}
+# summa = 0
+# for i in numbers:
+#     i ** 2
+# print(sum(numbers))
+
+
+##8.4.4
+# fruits = {'apple', 'banana', 'cherry', 'avocado', 'pineapple', 'apricot', 'banana', 'avocado', 'grapefruit'}
+# sorted_fruits = sorted(fruits, reverse=True)
+# print(*sorted_fruits, sep="\n")
+
+#8.4.7
+# s1 = input()
+# s2 = input()
+# if set([str(i) for i in range(10)]) == set(s1 + s2):
+#     print("YES")
+# else:
+#     print("NO")
+
+
+#8.4.8
+# if set(input()) == set(input()):
+#     print("YES")
+# else:
+#     print("NO")
+
+#8.4.9
+# s = input().split()
+# if set(s[0]) == set(s[1]) == set(s[2]):
+#     print("YES")
+# else:
+#     print("NO")
+
+## 8.5.1
+# n = int(input())
+# for i in range(n):
+#     print(len(set(input().lower())))
+
+
+## 8.5.2
+# n = int(input())
+# s = ''
+# for i in range(n):
+#     s += input().lower()
+# print(len(set(s)))
+
+
+## 8.5.3
+# text = input()
+# s = [word.lower().strip('.,;:-?!') for word in text.split()]
+# print(len(set(s)))
+
+
+## 8.5.3
+# s = [int(i) for i in input().split()]
+# print("NO")
+# for i in range(1, len(s)):
+#     if s[i] in s[:i]:
+#         print("YES")
+#     else:
+#         print("NO")
+
+
+## 8.6.
+
+## НЕИЗМЕНЯЮЩИЕ МЕТОДЫ
+# myset1 = {1, 2, 3, 4, 5}  #объедтнение множеств
+# myset2 = {3, 4, 6, 7, 8}
+#
+# myset3 = myset1 | myset2
+# print(myset3)
+# myset1 = myset1.union(myset2)
+# print(myset1)
+
+
+# myset1 = {1, 2, 3, 4, 5}  #пересечение множеств
+# myset2 = {3, 4, 6, 7, 8}
+
+# myset3 = myset1.intersection(myset2)
+# myset3 = myset1 & myset2
+# print(myset3)
+
+
+# myset1 = {1, 2, 3, 4, 5} #разность множеств #отличающиеся элементы одного множества
+# myset2 = {3, 4, 6, 7, 8}
+# #
+# # myset3 = myset1.difference(myset2)
+# myset3 = myset1- myset2
+# print(myset3)
+
+
+# myset1 = {1, 2, 3, 4, 5} #отличающиеся элементы обоих множеств
+# myset2 = {3, 4, 6, 7, 8}
+#
+# # myset3 = myset1.symmetric_difference(myset2)
+# myset3 = myset1 ^ myset2
+# print(myset3)
 
 
 
+## ИЗМЕНЯЮЩИЕ МЕТОДЫ  |=, &=, -=, ^=
+
+# myset1 = {1, 2, 3, 4, 5}  #объедтнение множеств
+# myset2 = {3, 4, 6, 7, 8}
+#
+# myset1.update(myset2)
+# print(myset1)
 
 
+# myset1 = {1, 2, 3, 4, 5}  #объедтнение множеств
+# myset2 = {3, 4, 6, 7, 8}
+#
+# myset1.difference_update(myset2)
+# print(myset1)
 
 
+# myset1 = {1, 2, 3, 4, 5}  #объедтнение множеств
+# myset2 = {3, 4, 6, 7, 8}
+#
+# myset1.intersection_update(myset2)
+# print(myset1)
 
 
+# myset1 = {1, 2, 3, 4, 5}  #объедтнение множеств
+# myset2 = {3, 4, 6, 7, 8}
+#
+# myset1.symmetric_difference_update(myset2)
+# print(myset1)
 
 
+## 8.6.1
+# s1, s2 = set(input().split()), set(input().split())
+# print(len(s1 & s2))
 
+## 8.6.2
+# s1, s2 = set(input().split()), set(input().split())
+# s = [int(i) for i in (s1 & s2)]
+# print(*(sorted(s)))
 
+## 8.6.3
+# s1, s2 = set(input().split()), set(input().split())
+# s = [int(i) for i in (s1 - s2)]
+# print(*(sorted(s)))
 
+## 8.6.4
+n = int(input())
+s = set([int(i) for i in list(input())])
+my_set = s
+for i in range(n - 1):
+    s = set([int(i) for i in list(input())])
+    my_set &= s
+lst = list(my_set)
+print(*(sorted(lst)))
 
-
-
-
-
-
-
-
-
-
+# print(*sorted(lst))
 
 
 
